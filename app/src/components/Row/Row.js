1300f8d42944ed7abdcfce4787a3c5e3e05251d7
amitai <amitai@DESKTOP-VGM9SBF.localdomain>
2021-08-02 18:44:49 +0300

    server update every minute

--- app/src/components/Row/Row.js
+++ app/src/components/Row/Row.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { deleteChosenServer, getServersList, turnServerOnOF } from '../../services/service';
 export default function Row(props) {
+    let [runningTime, setTime] = useState(0);
+
     let data = props.data;
     let switchBtn = '';
-    console.log(data);
-    if(data.isRunning) {
+
+    if (data.isRunning) {
         switchBtn = 'Turn Off';
     }
     else {
@@ -18,15 +20,26 @@ export default function Row(props) {
         data.isRunning = !data.isRunning;
         turnServerOnOF(data).then(getServersList().then(list => props.setList([...list])));
     }
-    return(
+
+    useEffect(() => {
+        if (data.isRunning) {
+            const interval = setInterval(() => {
+                setTime(runningTime++);
+                console.log(runningTime);
+            }, 60000);
+            return () => clearInterval(interval);
+        }
+         // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    }, [])
+    return (
         <tr>
             <td>{data.ip}</td>
             <td>{data.name}</td>
-            <td>0</td> 
+            <td>{runningTime}</td>
             <td><button onClick={turnOnServer} type="button" className="btn btn-link">{switchBtn}</button></td>
             <td>{data.type.name}</td>
-            <td>{data.type.price}</td>
-            <td><button onClick= {deleteServer}>Delete</button></td>
+            <td>{data.type.price * runningTime}</td>
+            <td><button onClick={deleteServer}>Delete</button></td>
         </tr>
     )
 }
